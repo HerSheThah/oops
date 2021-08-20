26edb3b17cba28f354f71dcbad4840991be9ac82
HerSheThah <harshisubb510@gmail.com>
2021-08-21 01:16:50 +0530

    Create function_overloading.cpp

---
+++ function_overloading.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int max(int x, int y)
+{
+	if(x>y)
+		return x;
+	return y;
+}
+int max(int a, int b, int c)
+{
+	if(a>b)
+	{
+		if(a>c)
+			return a;
+		return c;
+	}
+	else{
+		if(b>c)
+			return b;
+		return c;
+	}
+}
+
+int main()
+{
+	cout<<max(4, 5)<<endl;
+	cout<<max(3, 8, 1);
+}
+
